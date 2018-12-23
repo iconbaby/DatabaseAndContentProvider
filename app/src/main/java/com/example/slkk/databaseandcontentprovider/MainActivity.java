7357b0955fafd35d83a23f7bf9e099e7b25ec19c
slkk <644187060@qq.com>
2018-12-23 17:42:10 +0800

    init database

--- app/src/main/java/com/example/slkk/databaseandcontentprovider/MainActivity.java
+++ app/src/main/java/com/example/slkk/databaseandcontentprovider/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.slkk.databaseandcontentprovider;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Context mContext;
+    private MyDatabaseHelper myDatabaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_createDatabase = findViewById(R.id.btn_click);
+        btn_createDatabase.setOnClickListener(this);
+
+        myDatabaseHelper = new MyDatabaseHelper(this, "book.db", null, 1);
+    }
+
+    @Override
+    public void onClick(View v) {
+        myDatabaseHelper.getWritableDatabase();
     }
 }
